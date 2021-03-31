2e2e9f237af21b0f4a20d35897791291b6738c8d
Linda Martinez Davis <lm62davis@outlook.com>
2021-03-31 15:19:41 -0400

    added resume

--- js/app.js
+++ js/app.js
@@ -1,50 +1,49 @@
 // Resize window based by browser size.
-window.addEventListener("resize", myFunction);
+window.addEventListener('resize', myFunction);
 
 const x = 0;
 function myFunction() {
   let txt = (x += 1);
-  document.getElementById("adjust").innerHTML = txt;
+  document.getElementById('adjust').innerHTML = txt;
 }
 
-const modal = document.getElementById("myModal");
+const modal = document.getElementById('myModal');
 
-const aboutBtn = document.getElementById("about");
+const aboutBtn = document.getElementById('about');
 
-const span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName('close')[0];
 
 aboutBtn.onclick = function () {
-  modal.style.display = "block";
-  aboutBtn.addClass("pullDown");
+  modal.style.display = 'block';
+  aboutBtn.addClass('pullDown');
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = "none";
+  modal.style.display = 'none';
 };
 
 // When the user clicks anywhere outside of the modal, close it
 // Need to fix - doesn't close
 window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   }
 };
-
 // function toggle() {
 //   const blur = document.getElementById("blur");
 //   blur.classList.toggle("active");
 //   const popup = document.getElementById("popup");
 //   popup.classList.toggle("active");
 // }
-const slider_img = document.querySelector(".slider-image");
+const slider_img = document.querySelector('.slider-image');
 let images = [
   // "FizzBuzzQix.png",
   // "BootCampBlog.png",
   // "DateNightApp.png",
   // "DonutMaker.png",
-  "FranklinSS.PNG",
-  "KaufmanSS.PNG",
+  'FranklinSS.PNG',
+  'KaufmanSS.PNG',
 ];
 let i = 0;
 
@@ -61,5 +60,5 @@ function next() {
 }
 
 function setImg() {
-  return slider_img.setAttribute("src", "images/" + images[i]);
+  return slider_img.setAttribute('src', 'images/' + images[i]);
 }
